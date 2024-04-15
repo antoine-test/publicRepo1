11bd96140acbf46fa346f7f451585d72e56eb8ad
Antoine Vigneau <antoine.vigneau@sonarsource.com>
2024-04-15 10:29:31 +0200

    F3 bis

--- src/f3.js
+++ src/f3.js
@@ -1,6 +1,3 @@
-function sum(xs) {
-  return xs.reduce((acc, current) => acc + current); // Noncompliant
-}
-console.log(sum([1, 2, 3, 4, 5])); // Prints 15
-console.log(sum([])); // TypeError: Reduce of empty array with no initial value
+const pi = 3.14;
+pi = 3.14159; // Noncompliant: TypeError: invalid assignment to const 'pi'
 
